3e1201319a1da3f4cbd79efe2f1bd8241f171c8f
dinhthong <nguyendinhthongit@gmail.com>
2022-05-25 00:09:51 +0700

    add qserial object and connect to USB UART device ok

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent),
+      ui(new Ui::MainWindow),
+      device(new QSerialPort(this))
 {
     ui->setupUi(this);
 }
@@ -17,4 +19,32 @@ MainWindow::~MainWindow()
 void MainWindow::on_btnConnect_clicked()
 {
     ui->lblSerial->setText("Hello world!");
+// if (ui->comboBoxDevices->count() == 0){
+//     this->addToLogs("No devices found.");
+//             return;
+// }
+ //QString portName = ui->comboBoxDevices->currentText().split(" ").first();
+ QString portName = "ttyUSB0";
+ this->device->setPortName(portName);
+ this->device->setBaudRate(QSerialPort::Baud19200);
+ this->device->setDataBits(QSerialPort::Data8);
+ this->device->setParity(QSerialPort::NoParity);
+ this->device->setStopBits(QSerialPort::OneStop);
+ this->device->setFlowControl(QSerialPort::NoFlowControl);
+
+ if(device->open(QIODevice::ReadWrite)){
+     qDebug() << "Port opened successfully. Setting the connection params...";
+     //this->addToLogs("UART enabled.");
+     qDebug() << "Writing down the parameters...";
+     qDebug() << "Baud rate:" << this->device->baudRate();
+     qDebug() << "Data bits:" << this->device->dataBits();
+     qDebug() << "Stop bits:" << this->device->stopBits();
+     qDebug() << "Parity:" << this->device->parity();
+     qDebug() << "Flow control:" << this->device->flowControl();
+     qDebug() << "Read buffer size:" << this->device->readBufferSize();
+     qDebug() << "Read buffer size:" << this->device->portName();
+     connect(this->device, SIGNAL(readyRead()), this, SLOT(readFromPort()));
+ } else {
+     //this->addToLogs("The port can not be opened.");
+ }
 }
